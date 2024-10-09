c5b25fd7f66b2f68ea0aba06d03e659e4f326fdf
ramilmavlihanov <Ramil040201@gmail.com>
2024-10-09 22:39:12 +0300

    module 7.2 done

---
+++ module_7.2.py
@@ -0,0 +1,24 @@
+
+def custom_write(file_name, strings):
+    file = open(file_name, 'a+', encoding="utf-8")
+    dict_to_return = {}
+    index = 1
+    for string in strings:
+        dict_to_return[(index, file.tell())] = string
+        file.write(string + "\n")
+        print(f"byte = {file.tell()}")
+        index += 1
+    file.close()
+    return dict_to_return
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
